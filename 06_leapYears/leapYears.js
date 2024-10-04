4819125b1d2a3415598638a2c2da72df58de2c13
Hao Wei Goh <haowei280297@gmail.com>
2024-10-04 17:08:18 +0800

    6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const IsNotYearDivisibleBy100 = year % 100 !== 0;
+    const IsYearDivisibleBy400 = year % 400 === 0;
 
+    if (isYearDivisibleByFour && (IsNotYearDivisibleBy100 || IsYearDivisibleBy400)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
