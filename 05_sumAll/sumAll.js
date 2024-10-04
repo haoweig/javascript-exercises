23562e5577261e687d5015a5cfb2d95fb239c040
Hao Wei Goh <haowei280297@gmail.com>
2024-10-04 17:01:43 +0800

    5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum=0;
+    if (typeof start !== 'number' || typeof end !== 'number' || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR";
+    } else if(start < 0 || end < 0){
+        return "ERROR";
+    } else if(start > end) {
+        for(let i=start;i>=end;i--){
+            sum += i;
+        }
+        return sum;
+    } else {
+        for(let i=start;i<=end;i++){
+            sum += i;
+        }
+        return sum;
+    }
 
 };
 
