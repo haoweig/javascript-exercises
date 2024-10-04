e1e3a462d1e095a0f33b255712cc55a76879b2c1
Hao Wei Goh <haowei280297@gmail.com>
2024-10-04 16:40:03 +0800

    1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(value, count) {
+    if (count === 0){
+        return '';
+    }
+    else if (count < 0){
+        return 'ERROR';
+    }
+    else{
+        let result = '';
+        for(let i=0; i<count; i++){
+            result += value;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
